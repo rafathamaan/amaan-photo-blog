1dea820bfb97d4bc74ef7a2660e305c21a2ecf6e
RafathAmaan Mohammad <116374643+rafathamaan@users.noreply.github.com>
2024-08-16 17:24:48 +0530

    3

--- src/photo/PhotoGridContainer.tsx
+++ src/photo/PhotoGridContainer.tsx
@@ -1,3 +1,83 @@
+// 'use client';
+
+// import SiteGrid from '@/components/SiteGrid';
+// import PhotoGrid from './PhotoGrid';
+// import PhotoGridInfinite from './PhotoGridInfinite';
+// import { clsx } from 'clsx/lite';
+// import AnimateItems from '@/components/AnimateItems';
+// import { ComponentProps, useCallback, useState } from 'react';
+
+// export default function PhotoGridContainer({
+//   cacheKey,
+//   photos,
+//   count,
+//   tag,
+//   camera,
+//   simulation,
+//   focal,
+//   animateOnFirstLoadOnly,
+//   header,
+//   sidebar,
+//   canSelect,
+// }: {
+//   cacheKey: string
+//   count: number
+//   header?: JSX.Element
+//   sidebar?: JSX.Element
+// } & ComponentProps<typeof PhotoGrid>) {
+//   const [
+//     shouldAnimateDynamicItems,
+//     setShouldAnimateDynamicItems,
+//   ] = useState(false);
+
+//   const onAnimationComplete = useCallback(() =>
+//     setShouldAnimateDynamicItems(true), []);
+
+//   const initialOffset = photos.length;
+
+//   return (
+//     <SiteGrid
+//       contentMain={<div className={clsx(
+//         header && 'space-y-8 mt-4',
+//       )}>
+//         {header &&
+//           <AnimateItems
+//             type="bottom"
+//             items={[header]}
+//             animateOnFirstLoadOnly
+//           />}
+//         <div className="space-y-0.5 sm:space-y-1">
+//           <PhotoGrid {...{
+//             photos,
+//             tag,
+//             camera,
+//             simulation,
+//             focal,
+//             animateOnFirstLoadOnly,
+//             onAnimationComplete,
+//             canSelect,
+//           }} />
+//           {count > initialOffset &&
+//             <PhotoGridInfinite {...{
+//               cacheKey,
+//               initialOffset,
+//               canStart: shouldAnimateDynamicItems,
+//               tag,
+//               camera,
+//               simulation,
+//               focal,
+//               animateOnFirstLoadOnly,
+//               canSelect,
+//             }} />}
+//         </div>
+//       </div>}
+//       contentSide={sidebar}
+//       sideHiddenOnMobile
+//     />
+//   );
+// }
+
+
 'use client';
 
 import SiteGrid from '@/components/SiteGrid';
@@ -17,62 +97,56 @@ export default function PhotoGridContainer({
   focal,
   animateOnFirstLoadOnly,
   header,
-  sidebar,
   canSelect,
 }: {
-  cacheKey: string
-  count: number
-  header?: JSX.Element
-  sidebar?: JSX.Element
+  cacheKey: string;
+  count: number;
+  header?: JSX.Element;
 } & ComponentProps<typeof PhotoGrid>) {
-  const [
-    shouldAnimateDynamicItems,
-    setShouldAnimateDynamicItems,
-  ] = useState(false);
+  const [shouldAnimateDynamicItems, setShouldAnimateDynamicItems] = useState(false);
 
-  const onAnimationComplete = useCallback(() =>
-    setShouldAnimateDynamicItems(true), []);
+  const onAnimationComplete = useCallback(() => setShouldAnimateDynamicItems(true), []);
 
   const initialOffset = photos.length;
 
   return (
     <SiteGrid
-      contentMain={<div className={clsx(
-        header && 'space-y-8 mt-4',
-      )}>
-        {header &&
-          <AnimateItems
-            type="bottom"
-            items={[header]}
-            animateOnFirstLoadOnly
-          />}
-        <div className="space-y-0.5 sm:space-y-1">
-          <PhotoGrid {...{
-            photos,
-            tag,
-            camera,
-            simulation,
-            focal,
-            animateOnFirstLoadOnly,
-            onAnimationComplete,
-            canSelect,
-          }} />
-          {count > initialOffset &&
-            <PhotoGridInfinite {...{
-              cacheKey,
-              initialOffset,
-              canStart: shouldAnimateDynamicItems,
-              tag,
-              camera,
-              simulation,
-              focal,
-              animateOnFirstLoadOnly,
-              canSelect,
-            }} />}
+      contentMain={
+        <div className={clsx(header && 'space-y-8 mt-4')}>
+          {header && (
+            <AnimateItems type="bottom" items={[header]} animateOnFirstLoadOnly />
+          )}
+          <div className="space-y-0.5 sm:space-y-1">
+            <PhotoGrid
+              {...{
+                photos,
+                tag,
+                camera,
+                simulation,
+                focal,
+                animateOnFirstLoadOnly,
+                onAnimationComplete,
+                canSelect,
+              }}
+            />
+            {count > initialOffset && (
+              <PhotoGridInfinite
+                {...{
+                  cacheKey,
+                  initialOffset,
+                  canStart: shouldAnimateDynamicItems,
+                  tag,
+                  camera,
+                  simulation,
+                  focal,
+                  animateOnFirstLoadOnly,
+                  canSelect,
+                }}
+              />
+            )}
+          </div>
         </div>
-      </div>}
-      contentSide={sidebar}
-      sideHiddenOnMobile
+      }
     />
   );
 }
