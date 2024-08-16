e9db8faa479b7a7ee8fa1248afd6fd118baba0c2
RafathAmaan Mohammad <116374643+rafathamaan@users.noreply.github.com>
2024-08-17 04:34:54 +0530

    Update PhotoLarge.tsx

--- src/photo/PhotoLarge.tsx
+++ src/photo/PhotoLarge.tsx
@@ -363,7 +363,7 @@ export default function PhotoLarge({
         setIsHovered(entry.isIntersecting);
       },
       {
-        threshold: 1.5, // Adjust as needed
+        threshold: 1.0, // Adjust as needed
       }
     );
 
