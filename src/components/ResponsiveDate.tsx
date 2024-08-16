7f641fcc9a8a062319318f6adaca2c65e4a03c43
RafathAmaan Mohammad <116374643+rafathamaan@users.noreply.github.com>
2024-08-16 20:28:37 +0530

    aaa

---
+++ src/components/ResponsiveDate.tsx
@@ -0,0 +1,19 @@
+import { formatDate } from '@/utility/date';
+import { clsx } from 'clsx/lite';
+
+export default function ResponsiveDate({
+  date,
+  className,
+  titleLabel,
+}: {
+  date: Date
+  className?: string
+  titleLabel?: string
+}) {
+  const title = titleLabel
+    ? `${titleLabel}: ${formatDate(date).toLocaleUpperCase()}`
+    : formatDate(date).toLocaleUpperCase();
+  return (
+    ('hello')
+  );
+}
