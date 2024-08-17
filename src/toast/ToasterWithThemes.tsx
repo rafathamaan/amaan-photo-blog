0fad8f8ca66a118b72f7e0f0e4c488e4d91dbd30
RafathAmaan Mohammad <116374643+rafathamaan@users.noreply.github.com>
2024-08-17 08:27:38 +0530

    Update ToasterWithThemes.tsx

--- src/toast/ToasterWithThemes.tsx
+++ src/toast/ToasterWithThemes.tsx
@@ -1,11 +1,16 @@
 'use client';
-
+import {useEffect} from 'react';
 import { clsx } from 'clsx/lite';
 import { useTheme } from 'next-themes';
-import { Toaster } from 'sonner';
+import { Toaster ,toast} from 'sonner';
 
 export default function ToasterWithThemes() {
   const { theme } = useTheme();
+  useEffect(() => {
+    toast.success('Welcome to our website! Please Use Search or Tags To Find The Required Images', {
+      duration: 5000, // 3 seconds
+    });
+  }, []);
   return (
     <Toaster
       theme={theme as 'system' | 'light' | 'dark'}
